740cc6e1459ba5cc756c866bab027dcc47df334a
sosaruben17 <41598898+sosaruben17@users.noreply.github.com>
2021-08-31 10:28:32 -0300

    Update ChallengeApplicationTests.java

--- src/test/java/com/winance/challenge/ChallengeApplicationTests.java
+++ src/test/java/com/winance/challenge/ChallengeApplicationTests.java
@@ -3,7 +3,7 @@ package com.winance.challenge;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ChallengeApplication.class)
 class ChallengeApplicationTests {
 
 	@Test
